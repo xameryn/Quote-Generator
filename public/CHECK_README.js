94b1a6d23e74b589ac585542df429c333427cd9f
xameryn <maximilianramann@gmail.com>
2024-07-07 23:26:49 -0700

    added support for multiple taxes/% based fees in person.js

--- public/CHECK_README.js
+++ public/CHECK_README.js
@@ -3,7 +3,9 @@ var personal = {
     name: 'Your Name',
     phone: '123.456.7890',
     email: 'email@mail.com',
-    address: ['Street & Address', 'City, Province/State']
+    address: ['Street & Address', 'City, Province/State'],
+    tax_names: ['GST', 'PST'],
+    tax_rates: [5, 7],
 }
 
 export { personal }

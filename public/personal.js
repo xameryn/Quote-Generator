209d54789eaff4673ad5b1f0091edfd790b65cae
xameryn <maximilianramann@gmail.com>
2024-07-07 22:56:31 -0700

    add demo personal.js

---
+++ public/personal.js
@@ -0,0 +1,9 @@
+var personal = {
+    company: 'Company Name',
+    name: 'Your Name',
+    phone: '123.456.7890',
+    email: 'email@gmail.com',
+    address: ['Street & Address', 'City, Province/State']
+}
+
+export { personal }

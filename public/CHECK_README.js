e34511ef014d10e829048a6517586acf65f2e081
xameryn <maximilianramann@gmail.com>
2024-07-07 23:55:37 -0700

    abstract foot/end note to personal.js for customization

--- public/CHECK_README.js
+++ public/CHECK_README.js
@@ -1,12 +1,19 @@
+var company = 'Company Name';
+var name = 'Your Name';
+var phone = '123.456.7890';
+var email = 'email@mail.com';
+
 var personal = {
-    company: 'Company Name',
-    name: 'Your Name',
-    phone: '123.456.7890',
-    email: 'email@mail.com',
-    address: ['Street & Address', 'City, Province/State'],
+    company: company,
+    name: name,
+    phone: phone,
+    email: email,
+    address: ['Street & Address', 'City, Province/State'], // Address line 1, Address line 2
     tax_names: ['GST', 'PST'],
     tax_rates: [5, 7],
-    end_note: 'Thank you for your business' // Appears at end of quote
+    end_note: `For any concerns, reach out to ${phone} or ${email}`, // Appears at end of quote
+    foot_note: 'Thank you for your business', // Appears below the end note in bold
+    document_type: 'Quotation' // For use in the title & 'Quotation Reference' etc.
 }
 
 export { personal }

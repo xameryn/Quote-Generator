ac2ad9eb079c091983fb3343551b10f8075dc5c4
xameryn <maximilianramann@gmail.com>
2024-09-02 14:31:15 -0700

    Added unit support and omission of tax when blank

--- public/CHECK_README.js
+++ public/CHECK_README.js
@@ -9,7 +9,8 @@ var personal = {
     phone: phone,
     email: email,
     address: ['Street & Address', 'City, Province/State'], // Address line 1, Address line 2
-    tax_names: ['GST', 'PST'],
+    units: '$', // Currency used for values (cost, hours, etc.)
+    tax_names: ['GST', 'PST'], // Leave as empty if no taxes
     tax_rates: [5, 7],
     end_note: `For any concerns, reach out to ${phone} or ${email}`, // Appears at end of quote
     foot_note: 'Thank you for your business', // Appears below the end note in bold
